348237f69d2a1ca06ed8674bc4b0d234bcefa40f
waiscodes <b.wais@outlook.com>
2021-01-18 18:17:59 -0700

    removed hello world component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
     <Upload />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import Upload from "../components/Upload";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
     Upload,
   },
 };
