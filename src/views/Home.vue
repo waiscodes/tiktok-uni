ed9e38b22899da629d0adbc93de1e2324f0843f0
waiscodes <b.wais@outlook.com>
2021-01-18 18:43:52 -0700

    rendered video on page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,27 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <Upload />
+    <VideoJSRecord />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Upload from "../components/Upload";
+import VideoJSRecord from "../components/VideoJSRecord.vue";
 
 export default {
   name: "Home",
   components: {
     Upload,
+    VideoJSRecord,
   },
 };
 </script>
+
+<style>
+#myVideo {
+  background-color: #95ddf5;
+  margin: auto;
+}
+</style>
