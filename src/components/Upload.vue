8f7f6c76e3f59685038ce2f6de69ef8b025ffeb7
waiscodes <b.wais@outlook.com>
2021-01-18 18:17:22 -0700

    added upload functionality rendered on the dom

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -17,3 +17,54 @@
     </div>
   </div>
 </template>
+
+<script>
+import firebase from "firebase";
+
+export default {
+  name: "Upload",
+  data() {
+    return {
+      imageData: null,
+      picture: null,
+      uploadValue: 0,
+    };
+  },
+  methods: {
+    previewImage(event) {
+      this.uploadValue = 0;
+      this.picture = null;
+      this.imageData = event.target.files[0];
+    },
+
+    onUpload() {
+      this.picture = null;
+      const storageRef = firebase
+        .storage()
+        .ref(`${this.imageData.name}`)
+        .put(this.imageData);
+      storageRef.on(
+        `state_changed`,
+        (snapshot) => {
+          this.uploadValue =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        },
+        (error) => {
+          console.log(error.message);
+        },
+        () => {
+          this.uploadValue = 100;
+          storageRef.snapshot.ref.getDownloadURL().then((url) => {
+            this.picture = url;
+          });
+        }
+      );
+    },
+  },
+};
+</script>
+<style scoped="">
+img.preview {
+  width: 200px;
+}
+</style>
