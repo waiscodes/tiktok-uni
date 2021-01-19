b7aff4335938747f6890ecb8883574ce399dce2f
waiscodes <b.wais@outlook.com>
2021-01-18 17:45:44 -0700

    added firebase to project and initialized it

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import firebase from "firebase";
 
-createApp(App).use(router).mount('#app')
+firebase.initializeApp({
+  apiKey: "AIzaSyCD9qHdiRGQBEFoyHy4BCijubMce8url60",
+  authDomain: "vue-upload-6a992.firebaseapp.com",
+  projectId: "vue-upload-6a992",
+  storageBucket: "vue-upload-6a992.appspot.com",
+  messagingSenderId: "436547538862",
+  appId: "1:436547538862:web:fb6e01811d1df2c7b1c23f",
+});
+
+createApp(App)
+  .use(router)
+  .mount("#app");
