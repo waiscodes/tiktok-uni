0411a1a07ce2b77704ee844d7b2e3193a3df633a
waiscodes <b.wais@outlook.com>
2021-01-18 17:47:11 -0700

    created an uploads component and scafolded it

---
+++ src/components/Upload.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>
+      <p>Upload an image to Firebase:</p>
+      <input type="file" @change="previewImage" accept="image/*" />
+    </div>
+    <div>
+      <p>
+        Progress: {{ uploadValue.toFixed() + "%" }}
+        <progress id="progress" :value="uploadValue" max="100"></progress>
+      </p>
+    </div>
+    <div v-if="imageData != null">
+      <img class="preview" :src="picture" />
+      <br />
+      <button @click="onUpload">Upload</button>
+    </div>
+  </div>
+</template>
